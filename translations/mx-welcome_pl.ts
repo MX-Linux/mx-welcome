4d7c1087135ce8b62a2e7ce9c33b53f54d60a736
Dolphin Oracle <dolphinoracle@gmail.com>
2022-07-19 22:59:41 -0400

    translation refresh

--- translations/mx-welcome_pl.ts
+++ translations/mx-welcome_pl.ts
@@ -1,223 +1,207 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.1" language="pl">
+<?xml version="1.0" ?><!DOCTYPE TS><TS version="2.1" language="pl">
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="../mainwindow.ui" line="14"/>
-        <location filename="../mainwindow.cpp" line="65"/>
-        <location filename="../mainwindow.cpp" line="431"/>
-        <location filename="../mainwindow.cpp" line="444"/>
+        <location filename="mainwindow.ui" line="14"/>
+        <location filename="mainwindow.cpp" line="65"/>
+        <location filename="mainwindow.cpp" line="255"/>
+        <location filename="mainwindow.cpp" line="264"/>
         <source>MX Welcome</source>
         <translation>MX Powitanie</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="80"/>
+        <location filename="mainwindow.ui" line="80"/>
         <source>Quit application</source>
         <translation>Zakończ aplikację</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="83"/>
+        <location filename="mainwindow.ui" line="83"/>
         <source>Close</source>
         <translation>Zamknij</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="90"/>
+        <location filename="mainwindow.ui" line="90"/>
         <source>Alt+N</source>
         <translation>Alt+N</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="112"/>
+        <location filename="mainwindow.ui" line="112"/>
         <source>About this application</source>
         <translation>O programie</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="115"/>
+        <location filename="mainwindow.ui" line="115"/>
         <source>About...</source>
         <translation>O...</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="122"/>
+        <location filename="mainwindow.ui" line="122"/>
         <source>Alt+B</source>
         <translation>Alt+B</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="203"/>
+        <location filename="mainwindow.ui" line="203"/>
         <source>Welcome</source>
-        <translation type="unfinished"></translation>
+        <translation>Witaj</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="484"/>
+        <location filename="mainwindow.ui" line="484"/>
         <source>Install MX Linux</source>
-        <translation type="unfinished"></translation>
+        <translation>Zainstaluj MX Linux</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="510"/>
+        <location filename="mainwindow.ui" line="510"/>
         <source>Tweak (Panel, etc...)</source>
         <translation>Ulepszenia (Panel, itp...)</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="536"/>
+        <location filename="mainwindow.ui" line="536"/>
         <source>Tools</source>
         <translation>Narzędzia</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="568"/>
+        <location filename="mainwindow.ui" line="568"/>
         <source>Wiki</source>
         <translation>Wiki</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="600"/>
+        <location filename="mainwindow.ui" line="600"/>
         <source>Users Manual</source>
         <translation>Podręcznik Użytkownika</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="626"/>
+        <location filename="mainwindow.ui" line="626"/>
         <source>FAQ</source>
         <translation>Często Zadawane Pytania</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="652"/>
+        <location filename="mainwindow.ui" line="652"/>
         <source>Tour</source>
-        <translation type="unfinished"></translation>
+        <translation>Wycieczka</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="674"/>
+        <location filename="mainwindow.ui" line="674"/>
         <source>Popular Apps</source>
         <translation>Popularne Aplikacje</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="700"/>
+        <location filename="mainwindow.ui" line="700"/>
         <source>Contribute</source>
         <translation>Wesprzyj</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="732"/>
+        <location filename="mainwindow.ui" line="732"/>
         <source>Videos</source>
         <translation>Wideo</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="764"/>
+        <location filename="mainwindow.ui" line="764"/>
         <source>Forums</source>
         <translation>Forum</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="790"/>
+        <location filename="mainwindow.ui" line="790"/>
         <source>About</source>
-        <translation>O programie</translation>
+        <translation>O systemie</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="798"/>
+        <location filename="mainwindow.ui" line="798"/>
         <source>SHORT SYSTEM REPORT:</source>
-        <translation type="unfinished"></translation>
+        <translation>KRÓTKI RAPORT SYSTEMOWY:</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="805"/>
+        <location filename="mainwindow.ui" line="805"/>
         <source>DESKTOP</source>
-        <translation type="unfinished"></translation>
+        <translation>PULPIT</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="819"/>
+        <location filename="mainwindow.ui" line="819"/>
         <source>Quick-System-Info Full Report</source>
-        <translation type="unfinished"></translation>
+        <translation>Pełny raport Quick-System-Info</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="829"/>
+        <location filename="mainwindow.ui" line="829"/>
         <source>Terms of Use</source>
         <translation>Warunki korzystania</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="846"/>
+        <location filename="mainwindow.ui" line="846"/>
         <source>The name “MX Linux” is covered by Linux Foundation Sublicense No. 20140605-0483. We develop software that is covered by a free license that can be examined in the Wiki list. We also include software developed by others that is under a free license.</source>
-        <translation type="unfinished"></translation>
+        <translation>Nazwa „MX Linux” jest objęta Sublicencją Linux Foundation nr 20140605-0483. Tworzymy oprogramowanie objęte wolną licencją, którą można sprawdzić na liście Wiki. Dołączamy również oprogramowanie stworzone przez innych, które jest objęte wolną licencją.</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="856"/>
+        <location filename="mainwindow.ui" line="856"/>
         <source>MX VERSION</source>
-        <translation type="unfinished"></translation>
+        <translation>WERSJA MX</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="863"/>
+        <location filename="mainwindow.ui" line="863"/>
         <source>DEBIAN VERSION:</source>
-        <translation type="unfinished"></translation>
+        <translation>WERSJA DEBIAN:</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="870"/>
+        <location filename="mainwindow.ui" line="870"/>
         <source>SUPPORTED UNTIL:</source>
-        <translation type="unfinished"></translation>
+        <translation>WSPARCIE DO:</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="890"/>
-        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
-&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
-p, li { white-space: pre-wrap; }
-&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
-&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:&apos;Noto Sans&apos;; font-size:10.5pt;&quot;&gt;&lt;br /&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="924"/>
+        <location filename="mainwindow.ui" line="924"/>
         <source>Show this dialog at start up</source>
         <translation>Pokaż to okno podczas uruchamiania</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="317"/>
+        <location filename="mainwindow.cpp" line="78"/>
         <source>User demo, password:</source>
         <translation>Użytkownik demo, hasło:</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="317"/>
+        <location filename="mainwindow.cpp" line="78"/>
         <source>Superuser root, password:</source>
         <translation>Superużytkownik root, hasło:</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="354"/>
-        <location filename="../mainwindow.cpp" line="370"/>
+        <location filename="mainwindow.cpp" line="178"/>
+        <location filename="mainwindow.cpp" line="194"/>
         <source>Error</source>
         <translation>Błąd</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="382"/>
-        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;&lt;span style=&quot; font-size:14pt; font-weight:600;&quot;&gt;%1 &amp;quot;%2&amp;quot;&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.cpp" line="431"/>
+        <location filename="mainwindow.cpp" line="255"/>
         <source>About MX Welcome</source>
         <translation>O programie MX Powitanie</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="432"/>
+        <location filename="mainwindow.cpp" line="255"/>
         <source>Version: </source>
         <translation>Wersja:</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="433"/>
+        <location filename="mainwindow.cpp" line="255"/>
         <source>Program for displaying a welcome screen in MX Linux</source>
         <translation>Program do wyświetlania ekranu powitalnego w MX Linux</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="435"/>
+        <location filename="mainwindow.cpp" line="255"/>
         <source>Copyright (c) MX Linux</source>
         <translation>Prawa autorskie © MX Linux</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="436"/>
+        <location filename="mainwindow.cpp" line="256"/>
         <source>License</source>
         <translation>Licencja</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="437"/>
+        <location filename="mainwindow.cpp" line="257"/>
         <source>Changelog</source>
         <translation>Dziennik zmian</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="438"/>
+        <location filename="mainwindow.cpp" line="258"/>
         <source>Cancel</source>
         <translation>Anuluj</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="454"/>
+        <location filename="mainwindow.cpp" line="274"/>
         <source>&amp;Close</source>
         <translation>&amp;Zamknij</translation>
     </message>
@@ -225,36 +209,32 @@ p, li { white-space: pre-wrap; }
 <context>
     <name>QApplication</name>
     <message>
+        <location filename="main.cpp" line="71"/>
         <source>You must run this program as normal user.</source>
-        <translation type="vanished">Musisz uruchomić ten program jako zwykły użytkownik.</translation>
+        <translation>Musisz uruchomić ten program jako zwykły użytkownik.</translation>
     </message>
 </context>
 <context>
     <name>QObject</name>
     <message>
-        <location filename="../main.cpp" line="44"/>
+        <location filename="main.cpp" line="44"/>
         <source>This tool displays a welcome screen with two tabs.</source>
-        <translation type="unfinished"></translation>
+        <translation>To narzędzie wyświetla ekran powitalny z dwiema zakładkami.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="47"/>
+        <location filename="main.cpp" line="47"/>
         <source>Start with About tab selected. The About tab provides basic information about the current MX Linux version, the user&apos;s hardware, and access to a full system report.</source>
-        <translation type="unfinished"></translation>
+        <translation>Zacznij od wybrania karty O systemie. Karta O systemie zawiera podstawowe informacje o bieżącej wersji systemu MX Linux, sprzęcie użytkownika oraz dostęp do pełnego raportu systemowego.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="50"/>
+        <location filename="main.cpp" line="50"/>
         <source>Run a test mode.</source>
-        <translation type="unfinished"></translation>
+        <translation>Uruchom tryb testowy.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="71"/>
+        <location filename="main.cpp" line="71"/>
         <source>Error</source>
         <translation>Błąd</translation>
-    </message>
-    <message>
-        <location filename="../main.cpp" line="71"/>
-        <source>You must run this program as normal user.</source>
-        <translation type="unfinished">Musisz uruchomić ten program jako zwykły użytkownik.</translation>
     </message>
 </context>
 </TS>
