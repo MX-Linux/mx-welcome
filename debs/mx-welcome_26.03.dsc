50614b2ec87e192062f6c8a1a156af2100f20d4a
Adrian <adrian@mxlinux.org>
2026-03-19 15:57:11 -0400

    New build

---
+++ debs/mx-welcome_26.03.dsc
@@ -0,0 +1,33 @@
+-----BEGIN PGP SIGNED MESSAGE-----
+Hash: SHA512
+
+Format: 3.0 (native)
+Source: mx-welcome
+Binary: mx-welcome
+Architecture: any
+Version: 26.03
+Maintainer: Adrian <adrian@mxlinux.org>
+Homepage: https://github.com/AdrianTM/mx-welcome
+Standards-Version: 3.9.8
+Vcs-Git: git://github.com/AdrianTM/mx-welcome
+Build-Depends: debhelper-compat (= 12), cmake (>= 3.16), ninja-build, qt6-base-dev, qt6-base-dev-tools, qt6-tools-dev, qt6-tools-dev-tools
+Package-List:
+ mx-welcome deb admin optional arch=any
+Checksums-Sha1:
+ 8244080a4269daa5e096973dc3167fd51b65bd87 96228 mx-welcome_26.03.tar.xz
+Checksums-Sha256:
+ 6a9e268436ab396a9c4fd484f2c5acdd1c64d6c165491bd2b05861adc7efc6c6 96228 mx-welcome_26.03.tar.xz
+Files:
+ 064785561c086bf4f03121b2cc6d7980 96228 mx-welcome_26.03.tar.xz
+
+-----BEGIN PGP SIGNATURE-----
+
+iQFHBAEBCgAxFiEE8ndToY6S45N+YzXncJOMeAZ57pgFAmm8VNoTHGFkcmlhbkBt
+eGxpbnV4Lm9yZwAKCRBwk4x4BnnumKGIB/4y5Bbx/CvVo8banzlwUFT/FEvmvXsn
+A+PrdrZExfaG1L3yoV5R1blPq0GAQbsNw9e48bIY09jdoyUrI/cYQ5CvP3RgrzZ/
+b84yQvGPU4jXdV8JHj3mNP6V/LU9N9jkSebyQXOw53he7GzS2vRplP1cBnb0jlCm
+ixaf/wq158Wr15ryeZ9jMtLve91QCVYA08FH8dE4vHX5Xem9kj2osQzSCcyy4Hx3
+G7y+nUFSnNhwB7Wc2+B+jrlVUqE7R+YPMe4Nq7GxhgDlzYvsWmyD4SMWbgmE8nnr
+ZkXbnrvB9poI/SBBskB1Jtg+a31cpN1Nhp5X10H+tXVZIoHHWM9vsB5/
+=gfBT
+-----END PGP SIGNATURE-----
--- translations/mx-welcome_en_US.ts
+++ translations/mx-welcome_en_US.ts
@@ -1,4 +1,4 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS version="2.1" language="ka">
+<?xml version="1.0" ?><!DOCTYPE TS><TS version="2.1" language="en_US">
 <context>
     <name>MainWindow</name>
     <message>
@@ -12,12 +12,12 @@
     <message>
         <location filename="mainwindow.ui" line="80"/>
         <source>Quit application</source>
-        <translation>აპლიკაციიდან გასვლა</translation>
+        <translation>Quit application</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="83"/>
         <source>Close</source>
-        <translation>დახურვა</translation>
+        <translation>Close</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="90"/>
@@ -27,12 +27,12 @@
     <message>
         <location filename="mainwindow.ui" line="112"/>
         <source>About this application</source>
-        <translation>ამ აპლიკაციის შესახებ</translation>
+        <translation>About this application</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="115"/>
         <source>About...</source>
-        <translation>შესახებ...</translation>
+        <translation>About...</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="122"/>
@@ -42,168 +42,168 @@
     <message>
         <location filename="mainwindow.ui" line="203"/>
         <source>Welcome</source>
-        <translation>მოგესალმებით</translation>
+        <translation>Welcome</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="484"/>
         <source>Install MX Linux</source>
-        <translation>MX Linux-ის დაყენება</translation>
+        <translation>Install MX Linux</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="510"/>
         <source>Tweak (Panel, etc...)</source>
-        <translation>მორგება (პანელი და სხვ...)</translation>
+        <translation>Tweak (Panel, etc...)</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="536"/>
         <source>Tools</source>
-        <translation>ხელსაწყოები</translation>
+        <translation>Tools</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="568"/>
         <source>Wiki</source>
-        <translation>ვიკი</translation>
+        <translation>Wiki</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="600"/>
         <source>Users Manual</source>
-        <translation>მომხმარებლის სახელმძღვანელო</translation>
+        <translation>Users Manual</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="626"/>
         <source>FAQ</source>
-        <translation>ხდკ</translation>
+        <translation>FAQ</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="652"/>
         <source>Tour</source>
-        <translation>ტური</translation>
+        <translation>Tour</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="674"/>
         <source>Popular Apps</source>
-        <translation>პოპულარული პროგრამები</translation>
+        <translation>Popular Apps</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="700"/>
         <source>Contribute</source>
-        <translation>წვლილი</translation>
+        <translation>Contribute</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="732"/>
         <source>Videos</source>
-        <translation>ვიდეოები</translation>
+        <translation>Videos</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="764"/>
         <source>Forums</source>
-        <translation>ფორუმები</translation>
+        <translation>Forums</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="790"/>
         <source>About</source>
-        <translation>შესახებ</translation>
+        <translation>About</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="798"/>
         <source>SHORT SYSTEM REPORT:</source>
-        <translation>სისტემის მოკლე ანგარიში:</translation>
+        <translation>SHORT SYSTEM REPORT:</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="805"/>
         <source>DESKTOP</source>
-        <translation>სამუშაო მაგიდა</translation>
+        <translation>DESKTOP</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="819"/>
         <source>Quick-System-Info Full Report</source>
-        <translation>სწრაფი-სისტემის-ინფორმაციის სრული ანგარიში</translation>
+        <translation>Quick-System-Info Full Report</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="829"/>
         <source>Terms of Use</source>
-        <translation>გამოყენების პირობები</translation>
+        <translation>Terms of Use</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="846"/>
         <source>The name “MX Linux” is covered by Linux Foundation Sublicense No. 20140605-0483. We develop software that is covered by a free license that can be examined in the Wiki list. We also include software developed by others that is under a free license.</source>
-        <translation>სახელი &quot;MX Linux&quot; დაცულია Linux Foundation-ის ქველიცენზიით No. 20140605-0483. ჩვენ ვწერთ პროგრამას, რომელიც დაცულია თავისუფალი ლიცენზიით, რომლის შემოწმებას ვიკის სიიდან შეგიძლიათ. ჩვენ ასევე გვაქვს პროგრამები, რომლებიც დაწერილია სხვების მიერ, მაგრამ ასევე ვრცელდება თავისუფალი ლიცენზიით.</translation>
+        <translation>The name “MX Linux” is covered by Linux Foundation Sublicense No. 20140605-0483. We develop software that is covered by a free license that can be examined in the Wiki list. We also include software developed by others that is under a free license.</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="856"/>
         <source>MX VERSION</source>
-        <translation>MX-ის ვერსია</translation>
+        <translation>MX VERSION</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="863"/>
         <source>DEBIAN VERSION:</source>
-        <translation>DEBIAN-ის ვერსია:</translation>
+        <translation>DEBIAN VERSION:</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="870"/>
         <source>SUPPORTED UNTIL:</source>
-        <translation>მხარდაჭერის დრო:</translation>
+        <translation>SUPPORTED UNTIL:</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="924"/>
         <source>Show this dialog at start up</source>
-        <translation>ამ დიალოგის ჩვენება გაშვებისას</translation>
+        <translation>Show this dialog at start up</translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="78"/>
         <source>User demo, password:</source>
-        <translation>მომხმარებელი demo, პაროლი:</translation>
+        <translation>User demo, password:</translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="78"/>
         <source>Superuser root, password:</source>
-        <translation>ზემომხმარებელი root, parole:</translation>
+        <translation>Superuser root, password:</translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="178"/>
         <location filename="mainwindow.cpp" line="194"/>
         <source>Error</source>
-        <translation>შეცდომა</translation>
+        <translation>Current kernel doesn&apos;t support selected compression algorithm, please edit the configuration file and select a different algorithm.</translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="255"/>
         <source>About MX Welcome</source>
-        <translation>MX Welcome-ის შესახებ</translation>
+        <translation>About MX Welcome</translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="255"/>
         <source>Version: </source>
-        <translation>ვერსია: </translation>
+        <translation>Version: </translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="255"/>
         <source>Program for displaying a welcome screen in MX Linux</source>
-        <translation>პროგრამა მისალმების ეკრანის საჩვენებლად MX Linux-ში</translation>
+        <translation>Program for displaying a welcome screen in MX Linux</translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="255"/>
         <source>Copyright (c) MX Linux</source>
-        <translation>(c) MX Linux საავტორო ფულებები დაცულია</translation>
+        <translation>Copyright (c) MX Linux</translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="256"/>
         <source>License</source>
-        <translation>ლიცენზია</translation>
+        <translation>License</translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="257"/>
         <source>Changelog</source>
-        <translation>ცვლილებების ჟურნალი</translation>
+        <translation>Changelog</translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="258"/>
         <source>Cancel</source>
-        <translation>გაუქმება</translation>
+        <translation>Cancel</translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="274"/>
         <source>&amp;Close</source>
-        <translation>&amp;დახურვა</translation>
+        <translation>&amp;Close</translation>
     </message>
 </context>
 <context>
@@ -211,7 +211,7 @@
     <message>
         <location filename="main.cpp" line="71"/>
         <source>You must run this program as normal user.</source>
-        <translation>ეს პროგრამა ნორმალური მომხმარებლით უნდა გაუშვათ.</translation>
+        <translation>You must run this program as normal user.</translation>
     </message>
 </context>
 <context>
@@ -219,22 +219,22 @@
     <message>
         <location filename="main.cpp" line="44"/>
         <source>This tool displays a welcome screen with two tabs.</source>
-        <translation>ეს პროგრამა აჩვენებს მისალმების ეკრანს ორი ჩანართით.</translation>
+        <translation>This tool displays a welcome screen with two tabs.</translation>
     </message>
     <message>
         <location filename="main.cpp" line="47"/>
         <source>Start with About tab selected. The About tab provides basic information about the current MX Linux version, the user&apos;s hardware, and access to a full system report.</source>
-        <translation>გაშვება &apos;შესახებ&apos; ჩანართზე. ჩანართი &apos;შესახებ&apos; მოგაწვდით ძირითად ინფორმაციას მიმდინარე MX Linux-ის ვერსიის და მომხმარებლის აპარატურის შესახებ. ასევე წვდომას სრული სისტემის ანგარიშთან.</translation>
+        <translation>Start with About tab selected. The About tab provides basic information about the current MX Linux version, the user&apos;s hardware, and access to a full system report.</translation>
     </message>
     <message>
         <location filename="main.cpp" line="50"/>
         <source>Run a test mode.</source>
-        <translation>შემოწმების რეჟიმის გაშვება.</translation>
+        <translation>Run a test mode.</translation>
     </message>
     <message>
         <location filename="main.cpp" line="71"/>
         <source>Error</source>
-        <translation>შეცდომა</translation>
+        <translation>Current kernel doesn&apos;t support selected compression algorithm, please edit the configuration file and select a different algorithm.</translation>
     </message>
 </context>
 </TS>
